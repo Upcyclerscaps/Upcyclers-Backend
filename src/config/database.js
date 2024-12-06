07b9f84f1423baefb491b8109a855fcbbc9bc1a6
Zaidan Rivandani <74029884+zaidrvnd@users.noreply.github.com>
2024-12-06 19:22:53 +0700

    update

--- src/config/database.js
+++ src/config/database.js
@@ -1,22 +1,29 @@
-/* eslint-disable indent */
-/* eslint-disable linebreak-style */
-/* eslint-disable no-trailing-spaces */
 /* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
-/* eslint-disable prefer-const */
-/* eslint-disable space-before-function-paren */
 
 const mongoose = require('mongoose');
 const config = require('./config');
 
 const connectDB = async () => {
- try {
-   await mongoose.connect(config.MONGODB_URI);
-   console.log('MongoDB Connected');
- } catch (error) {
-   console.error('MongoDB connection error:', error);
-   process.exit(1);
- }
+  try {
+    await mongoose.connect(config.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+    console.log('MongoDB Connected');
+
+    mongoose.connection.on('error', (err) => {
+      console.error('MongoDB error:', err);
+    });
+
+    process.on('SIGINT', async () => {
+      await mongoose.connection.close();
+      process.exit(0);
+    });
+
+  } catch (error) {
+    console.error('MongoDB connection error:', error);
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
