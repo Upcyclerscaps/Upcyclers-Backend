cc95e2e063c832fe08cfaf49323ddea8ec0f0b29
Zaidan Rivandani <74029884+zaidrvnd@users.noreply.github.com>
2024-12-16 22:49:37 +0700

    update

--- src/app.js
+++ src/app.js
@@ -1,11 +1,11 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-undef */
 const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const mongoSanitize = require('express-mongo-sanitize');
 const xss = require('xss-clean');
-const https = require('https');
-const fs = require('fs');
 const config = require('./config/config');
 const connectDB = require('./config/database');
 const routes = require('./routes/index');
@@ -39,33 +39,23 @@ app.use('/api/v1/auth', authRoutes);
 app.use('/api/v1/products', productRoutes);
 app.use('/api/v1/users', userRoutes);
 app.use('/api/v1/uploads', uploadRoutes);
+
 app.use('/api/v1/buy-offers', buyOfferRoutes);
 
 // Error Handler
 app.use(errorHandler);
 
+const PORT = config.PORT;
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
+
 // CORS configuration
 app.use(cors({
-  origin: ['https://upcyclers.myvnc.com', 'https://localhost:5000'], // Update to allow your frontend domain
+  origin: ['http://localhost:9000', 'http://localhost:5000'], // tambahkan domain HTTP yang diizinkan
   credentials: true
 }));
-
-// Force all requests to be secure (for HTTPS redirection)
-app.use((req, res, next) => {
-  req.secure = true; // Mark all requests as secure
-  next();
-});
-
-// Read SSL certificate and key for HTTPS
-const sslOptions = {
-  cert: fs.readFileSync('/etc/letsencrypt/live/upcyclers.myvnc.com/fullchain.pem'),
-  key: fs.readFileSync('/etc/letsencrypt/live/upcyclers.myvnc.com/privkey.pem'),
-};
-
-// Start HTTPS server
-const PORT = config.PORT || 5000;
-https.createServer(sslOptions, app).listen(PORT, () => {
-  console.log(`Backend server is running on https://upcyclers.myvnc.com:${PORT}`);
-});
 
 module.exports = app;
